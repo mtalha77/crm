66d7c3c5a372cdc94c24d49113ab4abb961e7426
ShabiGardezi <shabigardezi51214@gmail.com>
2024-02-23 23:30:59 +0000

    business-view view-tickets

--- src/pages/view-tickets/index.tsx
+++ src/pages/view-tickets/index.tsx
@@ -1,5 +1,4 @@
 import { Typography } from '@mui/material'
-import React from 'react'
 import BusinessTicketsTable from 'src/layouts/components/tables/businessTicketsTable'
 
 function ViewTickets() {

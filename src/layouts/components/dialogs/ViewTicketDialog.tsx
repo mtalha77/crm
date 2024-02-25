f4b7edd60096f6b8bca14a3033358c61b700126b
Muhammad Hunfa Jalil <hunfa.jalil786@gmail.com>
2024-02-26 00:49:22 +0500

    change view ticket page to modal

--- src/layouts/components/dialogs/ViewTicketDialog.tsx
+++ src/layouts/components/dialogs/ViewTicketDialog.tsx
@@ -1,7 +1,87 @@
-import React from 'react'
+// ** React Imports
+import { Ref, useState, forwardRef, ReactElement } from 'react'
 
-function ViewTicketDialog() {
-  return <div>ViewTicketDialog</div>
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Dialog from '@mui/material/Dialog'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+
+import Fade, { FadeProps } from '@mui/material/Fade'
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+
+import Icon from 'src/@core/components/icon'
+import SinglePaymentHistory from '../singlePaymentHistory'
+import AddNewPayment from '../AddNewPayment'
+import { Slide, Tooltip, Icon as MuiIcon } from '@mui/material'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import ViewFullTicketDetails from '../ViewFullTicketDetails'
+
+const Transition = forwardRef(function Transition(
+  props: FadeProps & { children?: ReactElement<any, any> },
+  ref: Ref<unknown>
+) {
+  return <Slide direction='up' ref={ref} {...props} />
+})
+
+const ViewTicketDialog = (props: any) => {
+  // ** States
+  const [show, setShow] = useState<boolean>(false)
+  const { ticketId, depart } = props
+
+  return (
+    <>
+      <Tooltip title='View'>
+        <MuiIcon
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            setShow(true)
+          }}
+        >
+          <VisibilityIcon />
+        </MuiIcon>
+      </Tooltip>
+      <Dialog
+        fullScreen
+        open={show}
+        onClose={() => setShow(false)}
+        TransitionComponent={Transition}
+        onBackdropClick={() => setShow(false)}
+      >
+        <DialogContent
+          sx={{
+            backgroundColor: '#282A42',
+            position: 'relative',
+            pb: theme => `${theme.spacing(8)} !important`,
+            px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
+            pt: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
+          }}
+        >
+          <IconButton
+            size='small'
+            onClick={() => setShow(false)}
+            sx={{ position: 'absolute', right: '1rem', top: '1rem' }}
+          >
+            <Icon icon='mdi:close' />
+          </IconButton>
+
+          <ViewFullTicketDetails ticketId={ticketId} depart={depart} />
+        </DialogContent>
+        <DialogActions
+          sx={{
+            backgroundColor: '#282A42',
+            justifyContent: 'center',
+            px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
+            pb: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
+          }}
+        ></DialogActions>
+      </Dialog>
+    </>
+  )
 }
 
 export default ViewTicketDialog

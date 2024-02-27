369fdc282b64e7db4e55e7c1009e6a368e3cbb2d
Muhammad Hunfa Jalil <hunfa.jalil786@gmail.com>
2024-02-28 03:30:21 +0500

    fix

--- src/layouts/components/dialogs/UpdateBusinessDialog.tsx
+++ src/layouts/components/dialogs/UpdateBusinessDialog.tsx
@@ -17,7 +17,7 @@ import Icon from 'src/@core/components/icon'
 
 import { Slide, Tooltip, Icon as MuiIcon, Card, CardHeader, Divider, CardContent } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit'
-import BusinessUpdate from 'src/pages/business-update'
+import BusinessUpdate from 'src/layouts/components/business-update'
 
 const BoldText = ({ children }: any) => (
   <Typography variant='subtitle1' sx={{ fontWeight: 'bold', display: 'inline' }}>

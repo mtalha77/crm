27d9f9e08add1b506d51f78589ae5586d999ae06
Muhammad Hunfa Jalil <hunfa.jalil786@gmail.com>
2024-03-07 00:46:02 +0500

    j

--- check-node-version.ts
+++ check-node-version.ts
@@ -1,9 +1,9 @@
-const requiredVersion = 'v20.11.0'
-const actualVersion = process.version
+// const requiredVersion = 'v20.11.0'
+// const actualVersion = process.version
 
-if (actualVersion !== requiredVersion) {
-  console.error(
-    `Required Node.js version is ${requiredVersion}, but you are using ${actualVersion}. Please switch to the recommended version.`
-  )
-  process.exit(1)
-}
+// if (actualVersion !== requiredVersion) {
+//   console.error(
+//     `Required Node.js version is ${requiredVersion}, but you are using ${actualVersion}. Please switch to the recommended version.`
+//   )
+//   process.exit(1)
+// }

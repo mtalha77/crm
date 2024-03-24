b42cf91fab7aac54d79e8f97038b63409627f9de
Muhammad Hunfa Jalil <hunfa.jalil786@gmail.com>
2024-03-24 15:37:21 +0500

    ds

--- src/backend/utils/business-tickets/getAllTicketsForTeamLead.ts
+++ src/backend/utils/business-tickets/getAllTicketsForTeamLead.ts
@@ -1,7 +1,16 @@
+import mongoose from 'mongoose'
 import { BusinessTicketModel } from 'src/backend/schemas/businessTicket.schema'
 
 export const getAllTicketsTeamLead = async (req: any, res: any) => {
-  const tickets = await BusinessTicketModel.find({ assignee_depart_id: req.user.department_id })
+  let businessId = req.query.businessId
+
+  let filters = {}
+  if (businessId !== 'undefined') {
+    businessId = new mongoose.Types.ObjectId(businessId)
+
+    filters = { business_id: businessId }
+  }
+  const tickets = await BusinessTicketModel.find({ assignee_depart_id: req.user.department_id, ...filters })
     .populate({ path: 'business_id', select: 'business_name' })
     .populate({ path: 'assignee_employee_id', select: 'user_name' })
     .sort({ createdAt: -1 })

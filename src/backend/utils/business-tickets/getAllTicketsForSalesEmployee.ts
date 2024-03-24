fcc33745a7382bb53adb4a047de00b0f7a827200
Muhammad Hunfa Jalil <hunfa.jalil786@gmail.com>
2024-03-24 15:37:21 +0500

    ds

--- src/backend/utils/business-tickets/getAllTicketsForSalesEmployee.ts
+++ src/backend/utils/business-tickets/getAllTicketsForSalesEmployee.ts
@@ -2,7 +2,15 @@ import mongoose from 'mongoose'
 import { BusinessTicketModel } from 'src/backend/schemas/businessTicket.schema'
 
 export const getAllTicketsForSalesEmployee = async (req: any, res: any) => {
-  const tickets = await BusinessTicketModel.find({ created_by: new mongoose.Types.ObjectId(req.user._id) })
+  let businessId = req.query.businessId
+
+  let filters = {}
+  if (businessId !== 'undefined') {
+    businessId = new mongoose.Types.ObjectId(businessId)
+
+    filters = { business_id: businessId }
+  }
+  const tickets = await BusinessTicketModel.find({ created_by: new mongoose.Types.ObjectId(req.user._id), ...filters })
     .populate({ path: 'business_id', select: 'business_name' })
     .sort({ createdAt: -1 })
 
--- src/backend/utils/business-tickets/getAllTicketsForSalesManager.ts
+++ src/backend/utils/business-tickets/getAllTicketsForSalesManager.ts
@@ -1,7 +1,16 @@
+import mongoose from 'mongoose'
 import { BusinessTicketModel } from 'src/backend/schemas/businessTicket.schema'
 
 export const getAllTicketsForSalesManager = async (req: any, res: any) => {
-  const tickets = await BusinessTicketModel.find({})
+  let businessId = req.query.businessId
+
+  let filters = {}
+  if (businessId !== 'undefined') {
+    businessId = new mongoose.Types.ObjectId(businessId)
+
+    filters = { business_id: businessId }
+  }
+  const tickets = await BusinessTicketModel.find(filters)
     .populate({ path: 'business_id', select: 'business_name' })
     .sort({ createdAt: -1 })
 

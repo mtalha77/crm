2b9091cf40e88961ec6cfe74746db27b0329f730
ShabiGardezi <shabigardezi51214@gmail.com>
2024-04-24 21:17:28 +0000

    Added Hold Status in TicketStatus

--- src/shared/enums/TicketStatus.enum.ts
+++ src/shared/enums/TicketStatus.enum.ts
@@ -2,7 +2,8 @@ export enum TicketStatus {
   NOT_STARTED_YET = 'Not Started Yet',
   PENDING = 'Pending',
   IN_PROGRESS = 'In Progress',
-  COMPLETED = 'Completed'
+  COMPLETED = 'Completed',
+  HOLD = 'Hold'
 }
 
 export const TicketStatusValues: TicketStatus[] = Object.values(TicketStatus)

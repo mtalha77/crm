331c7a14578baab09efffb9ff925fe04e024b09c
ShabiGardezi <shabigardezi51214@gmail.com>
2024-06-21 00:09:50 +0500

    domain form backend

--- src/backend/schemas/domianform.schema.ts
+++ src/backend/schemas/domianform.schema.ts
@@ -2,19 +2,20 @@ import mongoose from 'mongoose'
 
 // schema for  Creation Date, Domain Name, Expiration Date, Price, Live Status, List Status .
 const domainFormSchema = new mongoose.Schema(
-
-    {
-        creation_date: {
-            type: Date,
-            default: Date.now
-        },
-        domain_name: { type: String, required: true },
-        expiration_date: { type: Date, },
-        price: { type: Number, required: true },
-        live_status: { type: String, },
-        list_status: { type: String, }
+  {
+    creation_date: {
+      type: Date,
+      default: Date.now
     },
-    { timestamps: true }
+    domain_name: { type: String, required: true },
+    expiration_date: { type: Date },
+    price: { type: Number, required: true },
+    live_status: { type: String },
+    list_status: { type: String },
+    domainApprovedBy: { type: String },
+    notes: { type: String }
+  },
+  { timestamps: true }
 )
 
 export const DomainFormModel = mongoose.models.DomainForm || mongoose.model('DomainForm', domainFormSchema)
